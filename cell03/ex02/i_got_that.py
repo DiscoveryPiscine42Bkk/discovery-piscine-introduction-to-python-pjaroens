bd509fcc74b0ed33a44fdd008a1ebd71b1640493
pjaroens <pichakorn144@gmail.com>
2025-06-19 01:41:34 +0700

    Create i_got_that.py

---
+++ cell03/ex02/i_got_that.py
@@ -0,0 +1,7 @@
+text = "-1"
+
+while True :
+    if text == "-1": text = str(input("What you gotta say? : "))
+    else : text = str(input("I got that! Anything else? : "))
+
+    if text == "STOP" :break;
