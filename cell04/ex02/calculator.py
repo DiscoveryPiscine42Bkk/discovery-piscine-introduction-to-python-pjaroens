daa168bf8c9b46a393a84c0b774331eeb6c6a469
pjaroens <pichakorn144@gmail.com>
2025-06-19 13:52:31 +0700

    Create calculator.py

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,9 @@
+f_num = int(input("Give me the first number: "))
+s_num = int(input("Give me the second number: "))
+
+print("Thank you!")
+
+print(f"{f_num} + {s_num} = {f_num + s_num}")
+print(f"{f_num} - {s_num} = {f_num - s_num}")
+print(f"{f_num} / {s_num} = {int(f_num / s_num)}")
+print(f"{f_num} * {s_num} = {f_num * s_num}")
